be53991a7e5b1aab3b7d25f68363aa1adf54dd1e
Sylvain Guérin <sylvain.guerin@ensta-bretagne.org>
2022-10-25 09:07:19 +0200

    MEDIUM / Unit tests

---
+++ diagram-ta-ui/src/test/java/org/openflexo/technologyadapter/diagram/gui/TestFMLEditorWithDiagramTA2.java
@@ -0,0 +1,204 @@
+/**
+ * 
+ * Copyright (c) 2014-2015, Openflexo
+ * 
+ * This file is part of Fml-technologyadapter-ui, a component of the software infrastructure 
+ * developed at Openflexo.
+ * 
+ * 
+ * Openflexo is dual-licensed under the European Union Public License (EUPL, either 
+ * version 1.1 of the License, or any later version ), which is available at 
+ * https://joinup.ec.europa.eu/software/page/eupl/licence-eupl
+ * and the GNU General Public License (GPL, either version 3 of the License, or any 
+ * later version), which is available at http://www.gnu.org/licenses/gpl.html .
+ * 
+ * You can redistribute it and/or modify under the terms of either of these licenses
+ * 
+ * If you choose to redistribute it and/or modify under the terms of the GNU GPL, you
+ * must include the following additional permission.
+ *
+ *          Additional permission under GNU GPL version 3 section 7
+ *
+ *          If you modify this Program, or any covered work, by linking or 
+ *          combining it with software containing parts covered by the terms 
+ *          of EPL 1.0, the licensors of this Program grant you additional permission
+ *          to convey the resulting work. * 
+ * 
+ * This software is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE. 
+ *
+ * See http://www.openflexo.org/license.html for details.
+ * 
+ * 
+ * Please contact Openflexo (openflexo-contacts@openflexo.org)
+ * or visit www.openflexo.org if you need additional information.
+ * 
+ */
+
+package org.openflexo.technologyadapter.diagram.gui;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.FileNotFoundException;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.openflexo.fml.controller.FMLFIBController;
+import org.openflexo.fml.controller.widget.fmleditor.FMLEditor;
+import org.openflexo.foundation.FlexoEditor;
+import org.openflexo.foundation.FlexoException;
+import org.openflexo.foundation.fml.FMLCompilationUnit;
+import org.openflexo.foundation.fml.VirtualModel;
+import org.openflexo.foundation.fml.VirtualModelLibrary;
+import org.openflexo.foundation.fml.rm.CompilationUnitResource;
+import org.openflexo.foundation.resource.FlexoResource;
+import org.openflexo.foundation.resource.FlexoResourceCenter;
+import org.openflexo.foundation.resource.ResourceLoadingCancelledException;
+import org.openflexo.foundation.technologyadapter.TechnologyAdapter;
+import org.openflexo.gina.ApplicationFIBLibrary.ApplicationFIBLibraryImpl;
+import org.openflexo.gina.swing.utils.FIBJPanel;
+import org.openflexo.gina.test.OpenflexoFIBTestCase;
+import org.openflexo.gina.test.SwingGraphicalContextDelegate;
+import org.openflexo.gina.utils.InspectorGroup;
+import org.openflexo.pamela.validation.ValidationReport;
+import org.openflexo.rm.Resource;
+import org.openflexo.rm.ResourceLocator;
+import org.openflexo.technologyadapter.diagram.DiagramTechnologyAdapter;
+import org.openflexo.test.OrderedRunner;
+import org.openflexo.test.TestOrder;
+import org.openflexo.test.UITest;
+
+/**
+ * Test {@link FMLEditor} component
+ * 
+ * @author sylvain
+ * 
+ */
+@RunWith(OrderedRunner.class)
+public class TestFMLEditorWithDiagramTA2 extends OpenflexoFIBTestCase {
+
+	private static SwingGraphicalContextDelegate gcDelegate;
+
+	private static Resource fibResource;
+
+	static FlexoEditor editor;
+
+	@BeforeClass
+	public static void setupClass() {
+		instanciateTestServiceManager(DiagramTechnologyAdapter.class);
+		initGUI();
+		for (FlexoResourceCenter<?> rc : serviceManager.getResourceCenterService().getResourceCenters()) {
+			System.out.println("**** RC " + rc);
+			for (FlexoResource<?> resource : rc.getAllResources()) {
+				System.out.println("   > " + resource.getURI());
+			}
+		}
+		for (TechnologyAdapter ta : serviceManager.getTechnologyAdapterService().getTechnologyAdapters()) {
+			System.out.println("**** TA " + ta + " loaded=" + ta.isActivated());
+		}
+	}
+
+	private static CompilationUnitResource fmlResource;
+
+	private static FMLCompilationUnit compilationUnit;
+
+	@Test
+	@TestOrder(3)
+	@Category(UITest.class)
+	public void loadFMLResource() throws FileNotFoundException, ResourceLoadingCancelledException, FlexoException {
+
+		VirtualModelLibrary vpLib = serviceManager.getVirtualModelLibrary();
+		assertNotNull(vpLib);
+		VirtualModel viewPoint = vpLib.getVirtualModel("http://openflexo.org/test/TestResourceCenter/TestControlledDiagramViewPoint.fml/TestVirtualModel.fml");
+		assertNotNull(viewPoint);
+
+		fmlResource = viewPoint.getResource();
+		assertNotNull(fmlResource);
+
+		compilationUnit = fmlResource.getCompilationUnit();
+
+	}
+
+	@Test
+	@TestOrder(4)
+	@Category(UITest.class)
+	public void testInstanciateFMLEditor() {
+
+		FMLEditor editor = new FMLEditor(fmlResource, null);
+		gcDelegate.addTab("FML Editor", editor);
+	}
+
+	@Test
+	@TestOrder(5)
+	@Category(UITest.class)
+	public void testInstanciateWidget() {
+
+		fibResource = ResourceLocator.locateResource("Fib/FML/CompilationUnitView.fib");
+		assertTrue(fibResource != null);
+		FIBJPanel<FMLCompilationUnit> widget = instanciateFIB(fibResource, fmlResource.getCompilationUnit(), FMLCompilationUnit.class);
+		FMLFIBController fibController = (FMLFIBController) widget.getController();
+		InspectorGroup fmlInspectorGroup = new InspectorGroup(ResourceLocator.locateResource("Inspectors/FML"),
+				ApplicationFIBLibraryImpl.instance(), null);
+		fibController.setDefaultInspectorGroup(fmlInspectorGroup);
+
+		// ModuleInspectorController inspectorController = new ModuleInspectorController(null);
+		// fibController.setInspectorController
+		gcDelegate.addTab("Standard GUI", widget.getController());
+	}
+
+	public static void initGUI() {
+		gcDelegate = new SwingGraphicalContextDelegate(TestFMLEditorWithDiagramTA2.class.getSimpleName());
+	}
+
+	@AfterClass
+	public static void waitGUI() {
+		gcDelegate.waitGUI();
+	}
+
+	@Before
+	public void setUp() {
+		gcDelegate.setUp();
+	}
+
+	@Override
+	@After
+	public void tearDown() throws Exception {
+		gcDelegate.tearDown();
+	}
+
+	@Test
+	@TestOrder(6)
+	@Category(UITest.class)
+	public void performSomeChecks() {
+
+		log("performSomeChecks");
+
+		/*assertEquals(5, compilationUnit.getVirtualModel().getFlexoProperties().size());
+		assertEquals(9, compilationUnit.getVirtualModel().getFlexoBehaviours().size());
+		assertEquals(1, compilationUnit.getVirtualModel().getFlexoConcepts().size());
+
+		foo = compilationUnit.getVirtualModel().getFlexoConcept("Foo");
+		assertNotNull(foo);
+		System.out.println("foo: " + foo);
+
+		assertNotNull(aRole = (PrimitiveRole<Integer>) compilationUnit.getVirtualModel().getAccessibleProperty("a"));
+		assertNotNull(aFooRole = (FlexoConceptInstanceRole) compilationUnit.getVirtualModel().getAccessibleProperty("aFoo"));
+		assertNotNull(allARole = (FlexoConceptInstanceRole) compilationUnit.getVirtualModel().getAccessibleProperty("allA"));
+		assertNotNull(vmDModelSlot = (FMLRTVirtualModelInstanceModelSlot) compilationUnit.getVirtualModel().getAccessibleProperty("vmD"));
+		assertNotNull(sequencingListProperty = (GetProperty<?>) compilationUnit.getVirtualModel().getAccessibleProperty("sequencingList"));
+*/
+		
+		ValidationReport validation = validate(compilationUnit);
+		assertEquals(0, validation.getErrorsCount());
+
+	}
+
+}
---
+++ diagram-ta/src/test/java/org/openflexo/technologyadapter/diagram/fml/TestFMLDiagramPrettyPrint1.java
@@ -0,0 +1,148 @@
+/**
+ * 
+ * Copyright (c) 2014, Openflexo
+ * 
+ * This file is part of Cartoeditor, a component of the software infrastructure 
+ * developed at Openflexo.
+ * 
+ * 
+ * Openflexo is dual-licensed under the European Union Public License (EUPL, either 
+ * version 1.1 of the License, or any later version ), which is available at 
+ * https://joinup.ec.europa.eu/software/page/eupl/licence-eupl
+ * and the GNU General Public License (GPL, either version 3 of the License, or any 
+ * later version), which is available at http://www.gnu.org/licenses/gpl.html .
+ * 
+ * You can redistribute it and/or modify under the terms of either of these licenses
+ * 
+ * If you choose to redistribute it and/or modify under the terms of the GNU GPL, you
+ * must include the following additional permission.
+ *
+ *          Additional permission under GNU GPL version 3 section 7
+ *
+ *          If you modify this Program, or any covered work, by linking or 
+ *          combining it with software containing parts covered by the terms 
+ *          of EPL 1.0, the licensors of this Program grant you additional permission
+ *          to convey the resulting work. * 
+ * 
+ * This software is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE. 
+ *
+ * See http://www.openflexo.org/license.html for details.
+ * 
+ * 
+ * Please contact Openflexo (openflexo-contacts@openflexo.org)
+ * or visit www.openflexo.org if you need additional information.
+ * 
+ */
+
+package org.openflexo.technologyadapter.diagram.fml;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openflexo.foundation.DefaultFlexoEditor;
+import org.openflexo.foundation.FlexoEditor;
+import org.openflexo.foundation.FlexoException;
+import org.openflexo.foundation.fml.FMLCompilationUnit;
+import org.openflexo.foundation.fml.VirtualModel;
+import org.openflexo.foundation.fml.VirtualModelLibrary;
+import org.openflexo.foundation.fml.parser.FMLParserTestCase;
+import org.openflexo.foundation.fml.parser.ParseException;
+import org.openflexo.foundation.fml.parser.fmlnodes.FMLCompilationUnitNode;
+import org.openflexo.foundation.fml.parser.fmlnodes.VirtualModelNode;
+import org.openflexo.foundation.fml.rm.CompilationUnitResource;
+import org.openflexo.foundation.resource.ResourceLoadingCancelledException;
+import org.openflexo.p2pp.RawSource;
+import org.openflexo.pamela.exceptions.ModelDefinitionException;
+import org.openflexo.test.OrderedRunner;
+import org.openflexo.test.TestOrder;
+
+/**
+ * Parse a FML file, perform some edits and checks that pretty-print is correct
+ * 
+ * @author sylvain
+ *
+ */
+@RunWith(OrderedRunner.class)
+public class TestFMLDiagramPrettyPrint1 extends FMLParserTestCase {
+
+	private static FMLCompilationUnit compilationUnit;
+	private static VirtualModel virtualModel;
+
+	static FlexoEditor editor;
+
+	private static CompilationUnitResource fmlResource;
+	private static VirtualModelNode vmNode;
+
+	@Test
+	@TestOrder(1)
+	public void initServiceManager() throws ParseException, ModelDefinitionException, IOException {
+		instanciateTestServiceManager();
+
+		editor = new DefaultFlexoEditor(null, serviceManager);
+		assertNotNull(editor);
+
+	}
+
+
+	@Test
+	@TestOrder(2)
+	public void loadInitialVersion() throws ParseException, ModelDefinitionException, IOException, ResourceLoadingCancelledException, FlexoException {
+		instanciateTestServiceManager();
+
+		log("Initial version");
+
+		/*for (FlexoResource<?> resource : serviceManager.getResourceManager().getRegisteredResources()) {
+			System.out.println(" > "+resource);
+		}
+		DiagramTechnologyAdapter diagramTechnologyAdapter = serviceManager.getTechnologyAdapterService().getTechnologyAdapter(DiagramTechnologyAdapter.class);
+		//serviceManager.getTechnologyAdapterService().activateTechnologyAdapter(diagramTechnologyAdapter,true);
+		for (FlexoResource<?> resource : serviceManager.getResourceManager().getRegisteredResources()) {
+			System.out.println(" > "+resource);
+		}*/
+		
+		VirtualModelLibrary vpLib = serviceManager.getVirtualModelLibrary();
+		assertNotNull(vpLib);
+		VirtualModel rootVM = vpLib.getVirtualModel("http://openflexo.org/test/TestResourceCenter/TestControlledDiagramViewPoint.fml/TestVirtualModel.fml");
+		assertNotNull(rootVM);
+		VirtualModel virtualModel = vpLib.getVirtualModel("http://openflexo.org/test/TestResourceCenter/TestControlledDiagramViewPoint.fml/TestVirtualModel.fml");
+		assertNotNull(virtualModel);
+
+		fmlResource = virtualModel.getResource();
+		assertNotNull(fmlResource);
+
+		compilationUnit = fmlResource.getCompilationUnit();
+
+		assertNotNull(virtualModel = compilationUnit.getVirtualModel());
+		assertEquals("TestVirtualModel", virtualModel.getName());
+
+		assertNotNull(rootNode = (FMLCompilationUnitNode) compilationUnit.getPrettyPrintDelegate());
+		assertNotNull(vmNode = (VirtualModelNode) rootNode.getObjectNode(virtualModel));
+
+		RawSource rawSource = rootNode.getRawSource();
+		System.out.println(rawSource.debug());
+		debug(rootNode, 0);
+
+		/*FlexoConcept transitionConcept = virtualModel.getFlexoConcept("Transition");
+		FlexoConceptNode conceptNode = (FlexoConceptNode) rootNode.getObjectNode(transitionConcept);
+		FlexoConceptInstanceRole incomings = (FlexoConceptInstanceRole) transitionConcept.getDeclaredProperty("incomings");
+		FlexoRolePropertyNode incomingsNode = (FlexoRolePropertyNode) rootNode.getObjectNode(incomings);
+		assertEquals("(28:2)-(28:92)", incomingsNode.getLastParsedFragment().toString());
+		FlexoConceptInstanceRole outgoings = (FlexoConceptInstanceRole) transitionConcept.getDeclaredProperty("outgoings");
+		FlexoRolePropertyNode outgoingsNode = (FlexoRolePropertyNode) rootNode.getObjectNode(outgoings);
+		assertEquals("(29:2)-(29:92)", outgoingsNode.getLastParsedFragment().toString());*/
+
+		System.out.println("FML=\n" + compilationUnit.getFMLPrettyPrint());
+		testFMLPrettyPrintEquals(compilationUnit, "TestFMLDiagramPrettyPrint1/Step1PrettyPrint.fml");
+
+		System.out.println("Normalized=\n" + compilationUnit.getNormalizedFML());
+		testNormalizedFMLRepresentationEquals(compilationUnit, "TestFMLDiagramPrettyPrint1/Step1Normalized.fml");
+
+	}
+
+}
